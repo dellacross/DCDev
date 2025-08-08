b4dd77145d5fd7b137695b6502fa9ae2a6b44909
dellacross <victordella652@gmail.com>
2025-08-08 09:16:21 -0300

    Edit styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,25 +3,25 @@ import { Braces, BriefcaseBusiness, Globe, GraduationCap, Lightbulb } from 'luci
 const Navbar = () => {
   return (
         <nav className='flex items-center justify-center gap-x-5 h-[60px] border-y-2 border-[#222222] px-8 box-border'>
-            <a href='#' className='flex text-white w-64 justify-center items-center gap-x-4 text-[#8D4BB9] underline underline-offset-4 decoration-2'>
+            <a href='#' className='flex text-[#46AD62] w-64 justify-center items-center gap-x-4 text-[#8D4BB9] underline underline-offset-4 decoration-2'>
                 <span><Lightbulb /></span>
-                <p>About Me</p>
+                <p>Sobre mim</p>
             </a>
-            <a href='#' className='flex text-white w-64 justify-center items-center gap-x-4 text-[#27A5F1] underline underline-offset-4 decoration-2'>
+            <a href='#' className='flex text-[#086099] w-64 justify-center items-center gap-x-4 text-[#27A5F1] underline underline-offset-4 decoration-2'>
                 <span><BriefcaseBusiness /></span>
-                <p>Carreer</p>
+                <p>Carreira</p>
             </a>
-            <a href='#' className='flex text-white w-64 justify-center items-center gap-x-4 text-[#F56565] underline underline-offset-4 decoration-2'>
+            <a href='#' className='flex text-[#C91634] w-64 justify-center items-center gap-x-4 text-[#F56565] underline underline-offset-4 decoration-2'>
                 <span><GraduationCap /></span>
-                <p>Educational</p>
+                <p>Educação</p>
             </a>
-            <a href='#' className='flex text-white w-64 justify-center items-center gap-x-4 text-[#70BD72] underline underline-offset-4 decoration-2'>
+            <a href='#' className='flex text-[#60368F] w-64 justify-center items-center gap-x-4 text-[#70BD72] underline underline-offset-4 decoration-2'>
                 <span><Globe /></span>
                 <p>Skills</p>
             </a>
-            <a href='/projects' className='flex text-white w-64 justify-center items-center gap-x-4 text-[#FFDF5E] underline underline-offset-4 decoration-2'>
+            <a href='/projects' className='flex text-[#DEA522] w-64 justify-center items-center gap-x-4 text-[#FFDF5E] underline underline-offset-4 decoration-2'>
                 <span><Braces /></span>
-                <p>Projects</p>
+                <p>Projetos</p>
             </a>
         </nav>
   )
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,9 +1,9 @@
 import type { ProjectType } from '../types/Project.type'
 
-const Project = ({ name, skills }: ProjectType) => {
+const Project = ({ name, skills, description }: ProjectType) => {
   return (
     <code className='w-full flex h-[25px] gap-x-2 items-center justify-between text-white text-sm'>
-      <div className='flex-1'>{ name }</div>
+      <div className='flex-1 overflow-hidden'>{`${name} - ${description}`}</div>
       <div className='flex gap-x-2'>
         {
           skills.map((skill, index) => (
