05133ffa29743f0221d70e42d6b04e91088be6cd
dellacross <victordella652@gmail.com>
2025-08-19 10:11:39 -0300

    Editing about tab - projects

--- src/components/Projects/About.tsx
+++ src/components/Projects/About.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'lucide-react'
 import { ProjectContext } from '../../pages/Projects'
 import { useContext } from 'react'
 
@@ -6,18 +7,21 @@ const About = () => {
     const { selectedProject } = useContext(ProjectContext)
 
     return (
-        <div className='flex flex-col gap-y-4 text-sm overflow-y-auto h-full'>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dolor magna, ullamcorper eu elementum id, aliquam a lectus. Cras sollicitudin tortor non erat consequat, et pharetra lorem aliquam. Phasellus imperdiet malesuada mi, non ornare urna viverra in. Sed ut iaculis est. Praesent non luctus est, et sodales risus. Suspendisse id facilisis mauris. Mauris gravida elit vel sapien eleifend, tincidunt euismod leo sagittis. Aliquam sollicitudin sapien nunc, non sodales dui suscipit ut. Cras suscipit nulla sed justo blandit, vitae tempor leo mollis. Proin ac lectus sed leo rhoncus imperdiet non non orci. Vivamus eu nulla ac lectus aliquam hendrerit ac sed lacus. Suspendisse pellentesque gravida turpis molestie mattis. Nullam id porttitor magna. Sed eget quam massa. Ut lectus metus, facilisis id volutpat at, consequat interdum tellus.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dolor magna, ullamcorper eu elementum id, aliquam a lectus. Cras sollicitudin tortor non erat consequat, et pharetra lorem aliquam. Phasellus imperdiet malesuada mi, non ornare urna viverra in. Sed ut iaculis est. Praesent non luctus est, et sodales risus. Suspendisse id facilisis mauris. Mauris gravida elit vel sapien eleifend, tincidunt euismod leo sagittis. Aliquam sollicitudin sapien nunc, non sodales dui suscipit ut. Cras suscipit nulla sed justo blandit, vitae tempor leo mollis. Proin ac lectus sed leo rhoncus imperdiet non non orci. Vivamus eu nulla ac lectus aliquam hendrerit ac sed lacus. Suspendisse pellentesque gravida turpis molestie mattis. Nullam id porttitor magna. Sed eget quam massa. Ut lectus metus, facilisis id volutpat at, consequat interdum tellus.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dolor magna, ullamcorper eu elementum id, aliquam a lectus. Cras sollicitudin tortor non erat consequat, et pharetra lorem aliquam. Phasellus imperdiet malesuada mi, non ornare urna viverra in. Sed ut iaculis est. Praesent non luctus est, et sodales risus. Suspendisse id facilisis mauris. Mauris gravida elit vel sapien eleifend, tincidunt euismod leo sagittis. Aliquam sollicitudin sapien nunc, non sodales dui suscipit ut. Cras suscipit nulla sed justo blandit, vitae tempor leo mollis. Proin ac lectus sed leo rhoncus imperdiet non non orci. Vivamus eu nulla ac lectus aliquam hendrerit ac sed lacus. Suspendisse pellentesque gravida turpis molestie mattis. Nullam id porttitor magna. Sed eget quam massa. Ut lectus metus, facilisis id volutpat at, consequat interdum tellus.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dolor magna, ullamcorper eu elementum id, aliquam a lectus. Cras sollicitudin tortor non erat consequat, et pharetra lorem aliquam. Phasellus imperdiet malesuada mi, non ornare urna viverra in. Sed ut iaculis est. Praesent non luctus est, et sodales risus. Suspendisse id facilisis mauris. Mauris gravida elit vel sapien eleifend, tincidunt euismod leo sagittis. Aliquam sollicitudin sapien nunc, non sodales dui suscipit ut. Cras suscipit nulla sed justo blandit, vitae tempor leo mollis. Proin ac lectus sed leo rhoncus imperdiet non non orci. Vivamus eu nulla ac lectus aliquam hendrerit ac sed lacus. Suspendisse pellentesque gravida turpis molestie mattis. Nullam id porttitor magna. Sed eget quam massa. Ut lectus metus, facilisis id volutpat at, consequat interdum tellus.
+        <div className='flex flex-col gap-y-4 text-sm overflow-y-auto h-full py-2'>
+            {
+                selectedProject?.link &&
+                <a 
+                    className='flex items-center rounded-md text-white size-max px-4'
+                    href={selectedProject?.link}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    <span className='bg-[#DEA522] flex items-center justify-center h-[30px] w-[30px] rounded-l-md'><Link size={16} /></span>
+                    <span className='px-2 text-white bg-[#262626] h-[30px] flex items-center rounded-r-md'>Acessar repositório</span>
+                </a>
+            }
+            <p className='text-sm hyphens-auto whitespace-pre-line'>
+                {selectedProject?.description}
             </p>
         </div>
     )
