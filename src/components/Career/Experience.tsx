36f96792323cf1021e02067f6da81cb40fe5d370
dellacross <victordella652@gmail.com>
2026-03-05 18:40:44 -0300

    Add profissional experience

--- src/components/Career/Experience.tsx
+++ src/components/Career/Experience.tsx
@@ -13,15 +13,15 @@ const Experience = () => {
       <div className='px-4 py-2 gap-4 flex max-md:flex-col'>
         <p className='flex items-center rounded-md text-white bg-[#262626]'>
           <span className='bg-[#086099] flex items-center justify-center h-[30px] w-[30px] rounded-l-md'><Building2 size={16} /></span>
-          <span className='px-2 bg-[#191919] h-[30px] rounded-r-md flex items-center text-sm'>{selectedExperience?.company}</span>
+          <span className='px-2 bg-[#191919] h-[30px] rounded-r-md flex items-center text-sm uppercase'>{selectedExperience?.company}</span>
         </p>
         <p className='flex items-center rounded-md text-white bg-[#262626]'>
           <span className='bg-[#086099] flex items-center justify-center h-[30px] w-[30px] rounded-l-md'><Clock size={16} /></span>
-          <span className='px-2 bg-[#191919] h-[30px] rounded-r-md flex items-center text-sm'>{selectedExperience?.duration}</span>
+          <span className='px-2 bg-[#191919] h-[30px] rounded-r-md flex items-center text-sm uppercase'>{selectedExperience?.duration}</span>
         </p>
         <p className='flex items-center rounded-md text-white bg-[#262626]'>
           <span className='bg-[#086099] flex items-center justify-center h-[30px] w-[30px] rounded-l-md'><Computer size={16} /></span>
-          <span className='px-2 bg-[#191919] h-[30px] rounded-r-md flex items-center text-sm'>{selectedExperience?.location}</span>
+          <span className='px-2 bg-[#191919] h-[30px] rounded-r-md flex items-center text-sm uppercase'>{selectedExperience?.location}</span>
         </p>
       </div>
       <div className={`flex text-lg px-4 py-2 bg-[#08609930] text-[#086099] min-h-[50px] items-center`}>
